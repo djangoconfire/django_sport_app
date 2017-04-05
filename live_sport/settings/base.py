63a7404e599637fd089f049f3f5cab8517ff93eb
djangoconfire <djangopycon@gmail.com>
2017-04-06 01:00:33 +0530

    updated dashboard.html

--- live_sport/settings/base.py
+++ live_sport/settings/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     #local apps
     'live_sport_app',
     'user_profile',
--- live_sport/settings/local.py
+++ live_sport/settings/local.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     # 'kombu.transport.django',
     #local apps
     'live_sport_app',

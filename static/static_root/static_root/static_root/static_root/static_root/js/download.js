d20f58036ff40c0be76d4032f849f417191b52ce
djangoconfire <djangopycon@gmail.com>
2017-04-05 16:58:59 +0530

    migrate staticfiles

---
+++ static/static_root/static_root/static_root/static_root/static_root/js/download.js
@@ -0,0 +1,33 @@
+var api_url='http://localhost:8000/api/order_list';
+var current_data;
+
+$.getJSON(api_url,function(data){
+    console.log(data);
+    $('#getcsv').click(function(){
+        download('data.csv', get_csv(data));
+    });
+});
+
+function get_csv(json){
+ var fields = Object.keys(json[0]);
+ var csv = json.map(function(row){
+   return fields.map(function(fieldName){
+     return JSON.stringify(row[fieldName] || '');
+   });
+ });
+ csv.unshift(fields);
+ return csv.join('\r\n');
+}
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
